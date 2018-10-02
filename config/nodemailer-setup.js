1c9f84b35acce67f91962a34598b843b8cf1bf5f
Pauline Brothier <paulinebrothier@gmail.com>
2018-10-02 10:30:03 +0200

    add form message and sticky menu

--- config/nodemailer-setup.js
+++ config/nodemailer-setup.js
@@ -1,25 +1,26 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transport = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.gmail_email,
-    pass: process.env.gmail_password,
-  },
-});
-
-
-function sendMessage(userDoc) {
-  const { fullName, email, message } = userDoc;
-
-  return transport.sendMail({
-    from: `${fullName} <${email}>`,
-    to: "OWNER MAIL",
-    subject: "New message from your website",
-    text: `${message}`,
-    html: `${message}`,
-  });
-}
+// const transport = nodemailer.createTransport({
+//   service: "Gmail",
+//   auth: {
+//     user: process.env.gmail_email,
+//     pass: process.env.gmail_password,
+//   },
+// });
 
 
-module.exports = { sendSignupMessage };
+// function sendMessage(userDoc) {
+
+//   const { fullName, email, message } = userDoc;
+
+//   return transport.sendMail({
+//     from: `${fullName} <${email}>`,
+//     to: "OWNER MAIL",
+//     subject: "New message from your website",
+//     text: `${message}`,
+//     html: `${message}`,
+//   });
+// }
+
+
+// module.exports = { sendSignupMessage };
--- routes/messages.js
+++ routes/messages.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
 const request = require('request');
 
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.gmail_email,
+    pass: process.env.gmail_password,
+  },
+});
 
 
 router.post('/send-email', (req, res, next) => {
 
 
-  let { email, subject, message } = req.body;
-  let transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'your email address',
-      pass: 'your email password'
-    }
-  });
-  transporter.sendMail({
-    from: '"My Awesome Project 👻" <myawesome@project.com>',
-    to: email,
-    subject: subject,
-    text: message,
-    html: `<b>${message}</b>`
+  let { fullName, email, message } = req.body;
+
+  transport.sendMail({
+    from: `${fullName} <${email}>`,
+    to: process.env.gmail_email,
+    subject: "New customer email from your website 🕶!",
+    text: `${message}`,
+    html: `
+    <h2>Hello Pauline!</h2>
+    <h3>You have just received a new message from ${fullName}.</h3>
+    <br />
+    <h3>Message</h3>
+    <p>${message}</p>
+    <br />
+    <p>You can respond to this mail : ${email}</p>
+    `,
   })
-    .then(info => res.render('message', { email, subject, message, info }))
+    .then(info => res.json(info))
     .catch(error => console.log(error));
 });
 
